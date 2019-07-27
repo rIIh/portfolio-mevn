96a3280069df018b5122f9ba44ab6e1620a6ed60
rIIh <ratehavchik@gmail.com>
2019-07-27 16:05:14 +0300

    Refactoring

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# My first script
-
-echo 'Removing old docker installation'
-sudo apt-get remove docker docker-engine docker.io containerd runc
-echo 'Installation docker'
-echo 'Done'
